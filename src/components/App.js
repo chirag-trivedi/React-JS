db35ccfcce4eb3c1c86433909051e56c946108d3
Chirag Trivedi <chirag.trivedi@gmail.com>
2021-08-01 16:06:09 -0400

    Class and Functional Components

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import Sample from './Sample';
+import Content from './Content';
+
+const App = () => {
+  return ( 
+      <div>
+  <h1>We are learning React</h1>
+  <Sample/>
+  <Content/>
+
+  </div>
+  )
+}
+
+export default App;
---
+++ src/components/Content.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Content = () => {
+    return (
+        <h1> Functional Component</h1>
+
+    )
+}
+
+export default Content;
